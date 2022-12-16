5e9226c534cade28ebefc419fb109a5e25696512
unknown <amsubhadeep@gmail.com>
2022-12-16 13:27:10 +0530

    Added Product data

--- src/database.js
+++ src/database.js
@@ -85,3 +85,365 @@ export const categoryData = [
       "https://res.cloudinary.com/deswqftqy/image/upload/v1663678287/Ecommerce%20Images/Category/Cycles_hnm5np.webp",
   },
 ];
+
+export const productData = [
+  {
+    productId: 1,
+    categoryId: 1,
+    productName: "Boult Audio Thunder Bluetooth Headset  (Black, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663915929/Ecommerce%20Images/Product/Headphone1_lran29.webp",
+    price: 799,
+    discount: 5,
+  },
+  {
+    productId: 2,
+    categoryId: 1,
+    productName:
+      "boAt Rockerz 510 Super Extra Bass Bluetooth Headset  (Furious blue, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663916022/Ecommerce%20Images/Product/Headphone2_lepc4l.webp",
+    price: 3999,
+    discount: 77,
+  },
+  {
+    productId: 3,
+    categoryId: 1,
+    productName:
+      "boAt Rockerz 450 Pro with Upto 70 Hours Playback Bluetooth Headset  (Aqua Blue, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663916096/Ecommerce%20Images/Product/Headphone3_glyryr.webp",
+    price: 1999,
+    discount: 23,
+  },
+  {
+    productId: 4,
+    categoryId: 1,
+    productName:
+      "JBL T450BT Extra Bass with Voice Assistant Support Lightweight Flat Foldable Bluetooth Headset  (Black, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663916276/Ecommerce%20Images/Product/Headphone4_sjox0r.webp",
+    price: 899,
+    discount: 0,
+  },
+  {
+    productId: 5,
+    categoryId: 1,
+    productName:
+      "Soundcore by Anker Life Q10 with Fast Charging Bluetooth Headset  (Black, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663916333/Ecommerce%20Images/Product/Headphone5_v3s86c.webp",
+    price: 1499,
+    discount: 15,
+  },
+  {
+    productId: 6,
+    categoryId: 1,
+    productName: "boAt Rockerz 370 Bluetooth Headset  (Fiery Red, On the Ear)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663916466/Ecommerce%20Images/Product/Headphone6_ak7seh.webp",
+    price: 1999,
+    discount: 20,
+  },
+  {
+    productId: 7,
+    categoryId: 2,
+    productName:
+      "Infinix X1 Series Core i7 10th Gen - (16 GB/512 GB SSD/Windows 11 Home/128 MB Graphics) XL12 Thin and Light Laptop  (14 Inch, Noble Red, 1.48 kg)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663924722/Ecommerce%20Images/Product/Laptop1_db2kdd.webp",
+    price: 59999,
+    discount: 8,
+  },
+  {
+    productId: 8,
+    categoryId: 2,
+    productName:
+      "RedmiBook 15 Core i3 11th Gen - (8 GB/256 GB SSD/Windows 10 Home) Thin and Light Laptop  (15.6 inch, Charcoal Gray, 1.8 kg, With MS Office)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663925277/Ecommerce%20Images/Product/Laptop2_rcuho0.webp",
+    price: 79999,
+    discount: 10,
+  },
+  {
+    productId: 9,
+    categoryId: 2,
+    productName:
+      "HP Pavilion Ryzen 5 Hexa Core AMD R5-5600H - (8 GB/512 GB SSD/Windows 10/4 GB Graphics/NVIDIA GeForce GTX 1650/144 Hz) 15-ec2004AX Gaming Laptop  (15.6 inch, Shadow Black, 1.98 kg)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663924722/Ecommerce%20Images/Product/Laptop3_l1kfyb.webp",
+    price: 49999,
+    discount: 2,
+  },
+  {
+    productId: 10,
+    categoryId: 2,
+    productName:
+      "ASUS ROG Strix G15 Ryzen 7 Octa Core AMD R7-4800H - (16 GB/1 TB SSD/Windows 11 Home/4 GB Graphics/NVIDIA GeForce RTX 3050 Ti/144 Hz) G513IE-HN040WS Gaming Laptop  (15.6 inch, Eclipse Gray, 2.10 kg, With MS Office)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663924722/Ecommerce%20Images/Product/Laptop4_rgv7un.webp",
+    price: 54999,
+    discount: 3,
+  },
+  {
+    productId: 11,
+    categoryId: 3,
+    productName: "SAMSUNG Galaxy F13 (Waterfall Blue, 64 GB)  (4 GB RAM)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663944697/Ecommerce%20Images/Product/Mobile1_x5bwz2.webp",
+    price: 24999,
+    discount: 5,
+  },
+  {
+    productId: 12,
+    categoryId: 3,
+    productName: "APPLE iPhone 11 (White, 128 GB)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663944771/Ecommerce%20Images/Product/Mobile2_l8wcxt.webp",
+    price: 24999,
+    discount: 8,
+  },
+  {
+    productId: 13,
+    categoryId: 3,
+    productName: "POCO C31 (Royal Blue, 32 GB)  (3 GB RAM)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663944847/Ecommerce%20Images/Product/Mobile3_bjgdxr.webp",
+    price: 39999,
+    discount: 15,
+  },
+  {
+    productId: 14,
+    categoryId: 4,
+    productName:
+      "boAt Stone 1200F Sunburn Edition with upto 9 Hours Playback and RGB LEDs 14 W Bluetooth Speaker  (Purple, Stereo Channel)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663944966/Ecommerce%20Images/Product/Speaker1_o3tw9p.webp",
+    price: 14999,
+    discount: 5,
+  },
+  {
+    productId: 15,
+    categoryId: 4,
+    productName:
+      "ZEBRONICS Zeb Vita Pro with TWS 24 W Bluetooth Soundbar  (Black, Stereo Channel)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663945041/Ecommerce%20Images/Product/Speaker2_lweic3.webp",
+    price: 18999,
+    discount: 13,
+  },
+  {
+    productId: 16,
+    categoryId: 4,
+    productName:
+      "ZEBRONICS BT4440 RUCF 60 Watt Bluetooth Home Theatre  (Black, 4.1 Channel)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663945149/Ecommerce%20Images/Product/Speaker3_uamyw9.webp",
+    price: 11999,
+    discount: 7,
+  },
+  {
+    productId: 17,
+    categoryId: 5,
+    productName: "Wauood Brown Color Ceramic Two Sides Open Flower Vase",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948491/Ecommerce%20Images/Product/Vase1_uhmsxp.webp",
+    price: 899,
+    discount: 23,
+  },
+  {
+    productId: 18,
+    categoryId: 5,
+    productName:
+      "Wauood Flower Pot Hand Painted Ceramic Flower Vase Sand Texture Decorative For Front Room And Gift Item 19 Cm (1) Ceramic Vase  (7.5 inch, Brown)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948561/Ecommerce%20Images/Product/Vase2_nthzts.webp",
+    price: 749,
+    discount: 15,
+  },
+  {
+    productId: 19,
+    categoryId: 5,
+    productName:
+      "Amaze Shine 24 INCH WHITE TREE IRON FLOWER VASE BIG SIZE FOR HOME AND OFFICE DECOR Iron Vase  (24 inch, White)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948620/Ecommerce%20Images/Product/Vase3_qtte5n.webp",
+    price: 1399,
+    discount: 12,
+  },
+  {
+    productId: 20,
+    categoryId: 6,
+    productName:
+      "Homefab India 213.5 cm (7 ft) Polyester Semi Transparent Door Curtain (Pack Of 2)  (Floral, Grey)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948724/Ecommerce%20Images/Product/Curtain1_g4joij.webp",
+    price: 699,
+    discount: 0,
+  },
+  {
+    productId: 21,
+    categoryId: 6,
+    productName:
+      "Adimanav 213.36 cm (7 ft) Polyester Blackout Door Curtain (Pack Of 3)  (Floral, Green, Off White)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948802/Ecommerce%20Images/Product/Curtain2_ou6dng.webp",
+    price: 699,
+    discount: 2,
+  },
+  {
+    productId: 22,
+    categoryId: 6,
+    productName:
+      "Ultica Fab 152 cm (5 ft) Polyester Window Curtain (Pack Of 4)  (Floral, Coffee Joint 4 Pc)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948852/Ecommerce%20Images/Product/Curtain3_wezvp6.webp",
+    price: 1999,
+    discount: 14,
+  },
+  {
+    productId: 23,
+    categoryId: 7,
+    productName:
+      "IWS 144 TC Microfiber Double Printed Flat Bedsheet  (Pack of 1, Green)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663948971/Ecommerce%20Images/Product/Bedsheet1_xyqkp8.webp",
+    price: 799,
+    discount: 12,
+  },
+  {
+    productId: 24,
+    categoryId: 7,
+    productName:
+      "IWS 144 TC Microfiber Double 3D Printed Flat Bedsheet  (Pack of 1, Blue)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949046/Ecommerce%20Images/Product/Bedsheet2_wpyj88.webp",
+    price: 1499,
+    discount: 23,
+  },
+  {
+    productId: 25,
+    categoryId: 7,
+    productName:
+      "Home Furnishing - Bed 144 TC Cotton Double Floral Flat Bedsheet  (Pack of 1, Blue)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949196/Ecommerce%20Images/Product/Bedsheet3_ouxpgd.webp",
+    price: 1999,
+    discount: 54,
+  },
+  {
+    productId: 26,
+    categoryId: 8,
+    productName:
+      "SR Handloom Grey, Yellow Polyester Carpet  (152 cm, X 60 cm, Rectangle)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949350/Ecommerce%20Images/Product/Mat1_ofr4jp.webp",
+    price: 299,
+    discount: 14,
+  },
+  {
+    productId: 27,
+    categoryId: 8,
+    productName:
+      "SPA Furnishing Beige Polyester Carpet  (182 cm, X 60 cm, Rectangle)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949412/Ecommerce%20Images/Product/Mat2_qkqluc.webp",
+    price: 599,
+    discount: 12,
+  },
+  {
+    productId: 28,
+    categoryId: 8,
+    productName:
+      "SPA Furnishing Red, Grey, Black Polyester Carpet  (182 cm, X 60 cm, Rectangle)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949497/Ecommerce%20Images/Product/Mat3_tx4r6x.webp",
+    price: 499,
+    discount: 0,
+  },
+  {
+    productId: 29,
+    categoryId: 9,
+    productName: "Adrenex by Flipkart GYM10LGREEN01  (Green, Kit Bag)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949814/Ecommerce%20Images/Product/GymBag1_gd0ax5.webp",
+    price: 2999,
+    discount: 35,
+  },
+  {
+    productId: 30,
+    categoryId: 9,
+    productName: "NIVIA Deflate Round -01  (Black, Kit Bag)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949856/Ecommerce%20Images/Product/GymBag2_e2eus2.webp",
+    price: 1599,
+    discount: 23,
+  },
+  {
+    productId: 31,
+    categoryId: 9,
+    productName: "NIVIA Beast-3 Gym Bag  (Multicolor, Kit Bag)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663949859/Ecommerce%20Images/Product/GymBag3_ffv5pb.webp",
+    price: 1599,
+    discount: 23,
+  },
+  {
+    productId: 32,
+    categoryId: 10,
+    productName: "COSCO Kick Football - Size: 5  (Pack of 1, Blue)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950042/Ecommerce%20Images/Product/Football1_c2wd7m.webp",
+    price: 1499,
+    discount: 31,
+  },
+  {
+    productId: 33,
+    categoryId: 10,
+    productName: "PUMA TeamFinal Wave Ball Football - Size: 5  (Pack of 1)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950158/Ecommerce%20Images/Product/Football2_tharjh.webp",
+    price: 1699,
+    discount: 28,
+  },
+  {
+    productId: 34,
+    categoryId: 10,
+    productName:
+      "ARMON Brazuca Football (Multicolour) with Pump Free Football - Size: 5  (Pack of 1)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950161/Ecommerce%20Images/Product/Football3_punglm.webp",
+    price: 1999,
+    discount: 19,
+  },
+  {
+    productId: 35,
+    categoryId: 11,
+    productName:
+      "Urban Terrain Denver with Complete Accessories, Free Home Installation & Mobile Tracking App 26 T Hybrid Cycle/City Bike  (Single Speed, Silver, Black)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950305/Ecommerce%20Images/Product/Cycle1_ltilz5.webp",
+    price: 6999,
+    discount: 58,
+  },
+  {
+    productId: 36,
+    categoryId: 11,
+    productName:
+      "Urban Terrain BOLT UT5000S27.5 Steel MTB-Disc Brakes,Free Installation & OneFitPlus App Tracking 27.5 T Mountain Cycle  (Single Speed, White)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950350/Ecommerce%20Images/Product/Cycle2_jhpv9i.webp",
+    price: 12000,
+    discount: 63,
+  },
+  {
+    productId: 37,
+    categoryId: 11,
+    productName:
+      "HRX Rustler with Integrated Carrier 85% Assembled 26 T Hybrid Cycle/City Bike  (Single Speed, Multicolor)",
+    imageUrl:
+      "https://res.cloudinary.com/deswqftqy/image/upload/v1663950393/Ecommerce%20Images/Product/Cycle3_r2ybru.webp",
+    price: 14000,
+    discount: 65,
+  },
+];
