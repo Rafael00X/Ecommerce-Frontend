7807f35856d78c2cd30233fb00ba3233452bbd23
unknown <amsubhadeep@gmail.com>
2022-12-20 13:23:22 +0530

    Refactored code

--- src/components/WarningModal.jsx
+++ src/components/WarningModal.jsx
@@ -1,49 +1,25 @@
-import ReactDOM from "react-dom";
+import Modal from "./Modal";
 
 export default function WarningModal(props) {
-  if (!props.isOpen) return null;
+  const { isOpen, message, onConfirm, onCancel, title } = props;
 
   return (
-    <>
-      {ReactDOM.createPortal(
-        <ModalElement params={props} />,
-        document.getElementById("modal")
-      )}
-    </>
-  );
-}
-
-function ModalElement(props) {
-  const {
-    params: { title, message, onConfirm, onCancel },
-  } = props;
-
-  //   return (
-  //     <>
-  //       <div className="modal-backdrop" />
-  //       <div className="modal-container">
-  //         <h1>Hello World!!!</h1>
-  //       </div>
-  //     </>
-  //   );
-
-  return (
-    <>
-      <div className="modal-backdrop" />
-      <div className="modal-container">
-        <div className="card w-50">
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{message}</p>
-            <button className="btn btn-info" onClick={onCancel}>
+    <Modal isOpen={isOpen}>
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <hr />
+          <p className="card-text">{message}</p>
+          <div className="d-flex justify-content-end">
+            <button className="btn btn-light" onClick={onCancel}>
               Cancel
             </button>
-            <button className="btn btn-primary" onClick={onConfirm}>
+            <button className="btn btn-dark" onClick={onConfirm}>
               Confirm
             </button>
           </div>
         </div>
       </div>
-    </>
+    </Modal>
   );
 }
